5604630e6bde6b9d3a9f6a3bb1bd4adb54297ba6
feedblackg44 <pilmyh@gmail.com>
2022-10-03 20:33:13 +0300

    correct navigation add

--- src/main/java/com/example/lab1/Lab1Controller.java
+++ src/main/java/com/example/lab1/Lab1Controller.java
@@ -10,4 +10,16 @@ public class Lab1Controller {
     public String index() {
         return "index.html";
     }
+    @RequestMapping(method = RequestMethod.GET, value = "/ilya")
+    public String indexI() {
+        return "../static/html/ilya.html";
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "/danylo")
+    public String indexB() {
+        return "../static/html/danylo.html";
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "/oleksii")
+    public String indexO() {
+        return "../static/html/oleksii.html";
+    }
 }
